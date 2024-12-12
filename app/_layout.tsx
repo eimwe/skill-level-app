916f5b21a1f174a7f7e385dd01de14f3b4315a92
eimwe <prokopenkoserge@inbox.ru>
2024-12-12 13:09:57 +0500

    Updated routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4f4f4",
+        },
+        headerTintColor: "#333",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    />
+  );
 }
